7a4f5ae07c824a9c22e375e7d71a0ad47f4584e8
Evoluke <evoluke.principal@gmail.com>
2025-11-24 13:08:30 -0300

    Add rarity-based monster skulls on spawn

---
+++ data/scripts/eventcallbacks/monster/poe_onSpawn.lua
@@ -0,0 +1,45 @@
+local ec = EventCallback
+
+local RARITY_SKULL_MAP = {
+    normal = SKULL_NONE,
+    magic = SKULL_GREEN,
+    rare = SKULL_YELLOW,
+    unique = SKULL_ORANGE,
+}
+
+local function resolveRarity(monster)
+    if not monster then
+        return nil
+    end
+
+    local rarity = monster.getRarity and monster:getRarity()
+    if rarity then
+        return rarity
+    end
+
+    if monster.getCustomAttribute then
+        local customRarity = monster:getCustomAttribute("poeRarity") or monster:getCustomAttribute("rarity")
+        if customRarity then
+            return customRarity
+        end
+    end
+
+    return nil
+end
+
+ec.onSpawn = function(self, position, startup, artificial)
+    self:registerEvent("PoeCombat")
+
+    local rarityKey = resolveRarity(self)
+    if rarityKey and type(rarityKey) == "string" then
+        local skullType = RARITY_SKULL_MAP[rarityKey:lower()]
+        if skullType then
+            self:setSkull(skullType)
+        end
+    end
+
+    return true
+end
+
+
+ec:register()
